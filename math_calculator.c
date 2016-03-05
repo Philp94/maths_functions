87767de1e171df60b70b0b03760d8b7e3d3b9d7a
Philp94 <b.philip1294@gmail.com>
2016-03-05 14:48:55 +0000

    Added files via upload

---
+++ math_calculator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+	int i, j, k, a, b;
+	int average = 5;	
+
+	printf("Enter 5 numbers to find average: ");
+	scanf("%i%i%i%i%i", &i, &j, &k, &a, &b);
+
+	printf("%i\n", i+j+k+a+b/average);
+	
+
+
+
+	//printf("%i * %i = %i\n", i, j, i*j); 
+	//printf("%i + %i = %i\n", i, j, i+j);
+	//printf("%i / %i = %i\n", i, j, i/j);
+	//printf("%i - %i = %i\n", i, j, i-j);
+
+}
+
