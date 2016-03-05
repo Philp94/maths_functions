9c9063ced69d5180a9242ad1ca772b361ed1f4f8
Ben Philip <b.philip1294@gmail.com>
2016-03-05 15:38:42 +0000

    Added complex min/max program into C code

--- math_calculator.c
+++ math_calculator.c
@@ -2,13 +2,23 @@
 int main()
 {
 	int i, j, k, a, b;
-	int average = 5;	
+	//int average = 5;	
+	int count;
 
-	printf("Enter 5 numbers to find average: ");
+	printf("Enter 5 numbers: ");
 	scanf("%i%i%i%i%i", &i, &j, &k, &a, &b);
 
-	printf("%i\n", i+j+k+a+b/average);
-	printf("Entered a trivial change\n");	
+	for(count = 0; count <= i; count++)	{
+		printf("%i", count);
+		return 0;
+	}
+
+	//printf("Enter 5 numbers to find average: ");
+	//scanf("%i%i%i%i%i", &i, &j, &k, &a, &b);
+
+	//printf("%i\n", i+j+k+a+b/average);
+	//printf("Entered a trivial change\n");	
+
 
 
 
